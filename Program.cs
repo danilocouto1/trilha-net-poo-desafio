7fcbb58e50f2619b6660b456d6fc988c9c0df516
Danilo <dansantosalfa@gmail.com>
2023-12-12 11:30:39 -0300

    Feat: Teste das Classes.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
 ﻿using DesafioPOO.Models;
+using System;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Teste para Nokia
+            Nokia nokiaPhone = new Nokia("123456", "IMEI123", "Nokia Model", 64);
+            nokiaPhone.Ligar();
+            nokiaPhone.ReceberLigacao();
+            nokiaPhone.InstalarAplicativo("WhatsApp");
+
+            // Teste para Iphone
+            Iphone iphone = new Iphone("654321", "IMEI456", "iPhone Model", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+
+            Console.ReadLine();
+        }
+    }
+}
