4f946906c2435e77905c688813b165498414e4df
Danilo <dansantosalfa@gmail.com>
2023-12-12 11:30:17 -0300

    Refarored: Acresentando o construtor das classes filhas.

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -3,6 +3,9 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Iphone : Smartphone
     {
+        public Iphone(string numero, string iMei, string modelo, int memoria)
+       : base(numero, iMei, modelo, memoria)
+        { }
         // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
         {
